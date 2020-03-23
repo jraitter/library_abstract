cdcd0123014ada44b71827670e6aa126da68d40d
jraitter <jraitter@cableone.net>
2020-03-23 17:13:47 -0600

    add and print ICheckaoutables

--- Models/Store.cs
+++ Models/Store.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using library_abstract.Interfaces;
 
@@ -10,6 +11,44 @@ namespace library_abstract.Models
     public List<Magazine> Magazines { get; set; }
     public List<Newspaper> NewsPapers { get; set; }
 
+    public void AddMag(Magazine newMag)
+    {
+      Magazines.Add(newMag);
+    }
+    public void AddPaper(Newspaper newPaper)
+    {
+      NewsPapers.Add(newPaper);
+    }
+    public void AddItem(ICheckoutable newItem)
+    {
+      Checkouts.Add(newItem);
+    }
+    public void AddItem(IEnumerable<ICheckoutable> newItems)
+    {
+      //add list of books
+      Checkouts.AddRange(newItems);
+    }
+
+    public void PrintAvailableItems()
+    {
+      for (int i = 0; i < Checkouts.Count; i++)
+      {
+        ICheckoutable item = Checkouts[i];
+        if (item is Book)
+        {
+          Book book = (Book)item;
+          Console.WriteLine($"{i + 1}. Type={book.PubType}: {book.Title} -- {book.Author} \n");
+        }
+      }
+    }
+
+    public Store(string address)
+    {
+      Address = address;
+      Checkouts = new List<ICheckoutable>();
+      Magazines = new List<Magazine>();
+      NewsPapers = new List<Newspaper>();
+    }
 
 
   }//endof class
--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using library_abstract.Interfaces;
+using library_abstract.Models;
 
 namespace library_abstract
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Hello World!");
+      Store nampa = new Store("123 Maint St.");
+      nampa.AddItem(new Book("Where The Sidewalk Ends", false, PubType.book, "Shel Silverstein"));
+
+      Book sw4 = new Book("Star Wars: A New Hope", false, PubType.book, "Ryder Windham");
+      Book sw5 = new Book("Star Wars: Empire Strikes Back", false, PubType.book, "Ryder Windham");
+      Book sw6 = new Book("Star Wars: Return of the Jedi", false, PubType.book, "Ryder Windham");
+
+      List<ICheckoutable> swSagabooks = new List<ICheckoutable>()
+      {
+        sw4,
+        sw5,
+        sw6
+      };
+      nampa.AddItem(swSagabooks);//using method overriding passing list
+
+      nampa.PrintAvailableItems();
     }
+  }
 }
